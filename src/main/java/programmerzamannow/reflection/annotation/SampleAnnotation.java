f008413a0d9990ee3c5a6f8dacd6d4f716fd5231
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-17 02:27:00 +0700

    package

---
+++ src/main/java/programmerzamannow/reflection/annotation/SampleAnnotation.java
@@ -0,0 +1,11 @@
+package programmerzamannow.reflection.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PACKAGE)
+public @interface SampleAnnotation {
+
+
+}
---
+++ src/main/java/programmerzamannow/reflection/data/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package ini berisikan data data
+ */
+
+@SampleAnnotation
+package programmerzamannow.reflection.data;
+
+import programmerzamannow.reflection.annotation.SampleAnnotation;
